002dbb1c12b3eb1b85aa5d483fd54b6ca6000a1e
DavidMoralesVega <moralesvegadavid@gmail.com>
2025-05-18 11:58:45 -0400

    feat: implement component button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from './componets/button/button.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [ButtonComponent],
 })
 export class AppComponent {
   title = 'login';
