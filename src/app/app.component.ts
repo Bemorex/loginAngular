801045b8916e191fdbe75f98acb4b050851e3528
DavidMoralesVega <moralesvegadavid@gmail.com>
2025-05-18 12:27:47 -0400

    feat: implement login-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from './components/button/button.component';
 import { InfoPanelComponent } from './components/info-panel/info-panel.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [InfoPanelComponent],
+  imports: [InfoPanelComponent, LoginFormComponent],
 })
 export class AppComponent {
   title = 'login';
---
+++ src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ButtonComponent } from '../button/button.component';
+
+email: FormControl<string | null>;
+password: FormControl<string | null>;
+remember: FormControl<boolean | null>;
+interface LoginForm {}
+@Component({
+  selector: 'app-login-form',
+  imports: [ButtonComponent, ReactiveFormsModule],
+  templateUrl: './login-form.component.html',
+})
+export class LoginFormComponent {
+  loginForm!: FormGroup<LoginForm>;
+  showPassword = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.loginForm = new FormGroup<LoginForm>({
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email],
+        nonNullable: false,
+      }),
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+        nonNullable: false,
+      }),
+      remember: new FormControl(false),
+    });
+  }
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  onSubmit(): void {
+    // Simulación de una solicitud de API
+    console.log('Form submitted:', this.loginForm.value);
+  }
+
+  onGoogleLogin($event: any): void {
+    console.log('Google login initiated', $event);
+    // Implementar lógica de login con Google
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+}
